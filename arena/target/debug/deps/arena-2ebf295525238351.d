487d9c440ba9bb5c84656820540f1dac26ac77e1
Hamza <official.hamzaderaoui@gmail.com>
2026-07-05 13:37:15 +0100

    Remove target folder

--- arena/target/debug/deps/arena-2ebf295525238351.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/RustProjects/ralloc/arena/target/debug/deps/arena-2ebf295525238351.d: src/main.rs
-
-/Users/mac/RustProjects/ralloc/arena/target/debug/deps/libarena-2ebf295525238351.rmeta: src/main.rs
-
-src/main.rs:
